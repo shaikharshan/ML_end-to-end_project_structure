48f47832e5b0c2ebbce16821b15c50432d7fdc74
arshan <arshan5446@gmail.com>
2024-06-24 16:07:39 +0530

    model training done,tested and best model found out

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,11 +7,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-# from src.components.data_transformation import DataTransformation
-# from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
-# from src.components.model_trainer import ModelTrainerConfig
-# from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import model_trainer_config
+from src.components.model_trainer import model_trainer
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -52,3 +54,7 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_path=train_data,test_path=test_data)
+    model_trainer_obj = model_trainer()
+    print(model_trainer_obj.initiate_model_trainer(train_arr=train_arr,test_arr=test_arr))
